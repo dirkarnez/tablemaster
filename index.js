48632b37acb0a98d6e5845dd6b67c96acabdcec7
Dirk Arnez <smalldirkalex@gmail.com>
2021-04-26 16:55:30 +0800

    - update index.js

--- index.js
+++ index.js
@@ -3,4 +3,10 @@
     .from(document.getElementById(id).rows)
     .filter((row, index) => !ignoreRowIdx.includes(index) && criteria(row))
     .forEach(row => row.style.backgroundColor = backgroundColor)
- })("timeTbl", [0], row => row.cells[3].innerText == "Individual" && row.cells[7].innerText == "Software", "yellow")
+ })("timeTbl", [0], row => {
+    try {
+        return row.cells[3].innerText == "Individual" && row.cells[7].innerText == "Software"
+    } catch (e) {
+        return false
+    }
+}, "yellow")
